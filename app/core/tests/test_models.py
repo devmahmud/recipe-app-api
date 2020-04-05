5d522664df33878b0e4f26776155ef0ec6afba81
Mahmudul Alam <expelmahmud@gmail.com>
2020-04-05 13:47:04 +0600

    Super user functionality added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
     def test_create_user_with_email(self):
         """
@@ -9,13 +10,12 @@ class ModelTest(TestCase):
         email = "test@gmail.com"
         password = "Test12345@"
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
-        )
+            email=email,
+            password=password
+            )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
 
     def test_user_email_normalized(self):
         """
@@ -25,3 +25,21 @@ class ModelTest(TestCase):
         user = get_user_model().objects.create_user(email, 'test12345')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_user_invalid_email(self):
+        """
+        Test creating user with no email raises error
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test12345')
+
+    def test_create_superuser(self):
+        """
+        Test Creating new superuser
+        """
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test12345'
+            )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)

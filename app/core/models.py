f9b40b2ac186fc7ee12abb5ed6ea6c4267a9874e
Mahmudul Alam <expelmahmud@gmail.com>
2020-04-11 11:01:04 +0600

    Upload image endpoint added

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,19 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """
+    Generate file path for new recipe image
+    """
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -79,6 +91,7 @@ class Recipe(models.Model):
         on_delete=models.CASCADE
     )
     title = models.CharField(max_length=255)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
